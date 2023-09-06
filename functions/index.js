c339b145435ecfa376453923238c3d6f8ba7c2af
Mike Angell <mike@evulse.com>
2023-09-06 14:01:03 +1000

    Change to JS

---
+++ functions/index.js
@@ -0,0 +1,24 @@
+/*
+ * This template contains a HTTP function that
+ * responds with a greeting when called
+ *
+ * Reference PARAMETERS in your functions code with:
+ * `process.env.<parameter-name>`
+ * Learn more about building extensions in the docs:
+ * https://firebase.google.com/docs/extensions/publishers
+ */
+
+const functions = require("firebase-functions");
+import Mux from '@mux/mux-node';
+const { Video } = new Mux(process.env.MUX_TOKEN_ID, process.env.MUX_TOKEN_SECRET);
+
+exports.greetTheWorld = functions.https.onRequest((req, res) => {
+  Video.Uploads.create({
+    cors_origin: process.env.MUX_CORS_URL, 
+    new_asset_settings: {
+      playback_policy: 'public'
+    }
+  }).then(upload => {
+    res.send(upload.url)
+  });
+});
